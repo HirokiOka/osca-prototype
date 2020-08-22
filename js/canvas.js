68cb6103ed1d0f1cde5f53d6ac56685b89424443
hiroki <hiro_bass.0827@icloud.com>
2020-08-22 22:52:03 +0900

    Initial commit

---
+++ js/canvas.js
@@ -0,0 +1,11 @@
+
+function setup() {
+    let canvas = createCanvas(560, 315);
+    canvas.parent('canvas');
+    background(0, 255, 0, 10);
+}
+
+function draw() {
+    background(0,0.1);
+    // circle(mouseX, mouseY, 10);
+}
